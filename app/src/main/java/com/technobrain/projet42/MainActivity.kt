b9b4efed05c51b72bf9b6ecdef7ef4fe89306431
Theodore MARION <117664928+Tetsu9901@users.noreply.github.com>
2024-07-04 08:57:36 +0200

    v2.1 (#16)
    * Tma map openstreetmap (#15)
    
    * detail screen + parcour sans map
    
    * detail et parcours
    
    * fix merge
    
    ---------
    
    Co-authored-by: sagaat <a.silvestre@lyon.ort.asso.fr>

--- app/src/main/java/com/technobrain/projet42/MainActivity.kt
+++ app/src/main/java/com/technobrain/projet42/MainActivity.kt
@@ -16,8 +16,8 @@ import androidx.navigation.compose.rememberNavController
 import com.technobrain.projet42.data.api.SessionManager
 import com.technobrain.projet42.domain.model.EventShort
 import com.technobrain.projet42.ui.document.DocumentScreen
-import com.technobrain.projet42.ui.home.EventScreen
 import com.technobrain.projet42.ui.event.MyMapPage
+import com.technobrain.projet42.ui.home.EventScreen
 import com.technobrain.projet42.ui.login.LoginForm
 import com.technobrain.projet42.ui.register.RegisterScreen
 import com.technobrain.projet42.ui.user.UserAccountScreen
